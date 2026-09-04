10711c3543961d0b4132cde89e1d45dfa60af08a
thisIsAyushFr <ayushrollno5@gmail.com>
2026-09-04 20:21:20 +0530

    feat: create PromptForge extension shell

---
+++ Extension/src/App.tsx
@@ -0,0 +1,21 @@
+function App(){
+  return(
+    <div className="w-80 p-6">
+
+      <h1 className="text-2x1 font-bold">
+        PromptForge
+      </h1>
+
+      <p className="mt-2 text-gray-600">
+        Improve your prompts
+      </p>
+
+      <button className="mt-6 w-full rounded-lg bg-black px-4 py-2 text-white">
+        Analyze Prompt
+      </button>
+
+    </div>
+  )
+}
+
+export default App;
